06ce77a869f7cf3be06361d19814bafdfa69b9c9
EgorSmi <Smirnov.egor.gena@mail.ru>
2021-02-12 14:35:09 +0300

    the first attempt,  hope i don't spoil anything

---
+++ src/execute/CMakeFiles/Execute.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/egor/Рабочий стол/afina/src/execute/Add.cpp" "/home/egor/Рабочий стол/afina/src/execute/CMakeFiles/Execute.dir/Add.cpp.o"
+  "/home/egor/Рабочий стол/afina/src/execute/Append.cpp" "/home/egor/Рабочий стол/afina/src/execute/CMakeFiles/Execute.dir/Append.cpp.o"
+  "/home/egor/Рабочий стол/afina/src/execute/Command.cpp" "/home/egor/Рабочий стол/afina/src/execute/CMakeFiles/Execute.dir/Command.cpp.o"
+  "/home/egor/Рабочий стол/afina/src/execute/Get.cpp" "/home/egor/Рабочий стол/afina/src/execute/CMakeFiles/Execute.dir/Get.cpp.o"
+  "/home/egor/Рабочий стол/afina/src/execute/Replace.cpp" "/home/egor/Рабочий стол/afina/src/execute/CMakeFiles/Execute.dir/Replace.cpp.o"
+  "/home/egor/Рабочий стол/afina/src/execute/Set.cpp" "/home/egor/Рабочий стол/afina/src/execute/CMakeFiles/Execute.dir/Set.cpp.o"
+  "/home/egor/Рабочий стол/afina/src/execute/Stats.cpp" "/home/egor/Рабочий стол/afina/src/execute/CMakeFiles/Execute.dir/Stats.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "src"
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/egor/Рабочий стол/afina/src/storage/CMakeFiles/Storage.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
